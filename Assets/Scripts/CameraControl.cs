b106e4dafb4c296515bf9c111ac9c12b6755dfba
Pruthvineo <pruthvi.raj@hotmail.com>
2017-04-07 16:25:45 -0400

    VR update

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	public float speed = 20.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis("Vertical") * speed;
+		transform.Translate (0,0, translation * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/ControlEve.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlEve : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		GameObject dialogue = GameObject.Find ("Dialogue");
+		dialogue.GetComponent<DialogueImplementation>().changeAnim += delegate(object sender, string animValue) {
+			if(animValue == "jump"){
+				anim.SetBool ("Jump", true);
+			}
+			else if(animValue == "death"){
+				anim.SetBool ("Death", true);
+			}
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
